32b3737e27103bc61d52d05e6a600a0f2e41432b
Elidon Morina <83674499+3mper0r@users.noreply.github.com>
2023-06-04 23:58:50 +0200

    tsconfig && types.d.ts

---
+++ types.d.ts
@@ -0,0 +1,12 @@
+interface Book {
+    id: string;
+    title: string;
+    first_published_year: number;
+    number_of_pages_median: number;
+    covers: {
+    S: string;
+    M: string;
+    L: string;
+    }
+    author_name: string
+}
